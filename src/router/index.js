39a62d0f85632c8726c5e36abea2d3d66108f7f7
Toluwalase Babatunde <toluwalasebabatunde@Toluwalases-MacBook-Pro.local>
2025-04-18 14:59:44 +0100

    custom 404 page

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,15 @@
 import { createBrowserRouter } from "react-router";
 import App from "../App";
+import ErrorPage from "../pages/404";
 
 const router = createBrowserRouter([
   {
     path: "/",
     Component: App,
+  },
+  {
+    path: "/404",
+    Component: ErrorPage,
   },
 ]);
 
