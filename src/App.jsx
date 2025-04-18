b543ea47564dd2edfa59728344666e7288cbf354
Toluwalase Babatunde <toluwalasebabatunde@Toluwalases-MacBook-Pro.local>
2025-04-18 14:53:15 +0100

    redux react-router tailwind setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,30 @@
+import { useDispatch, useSelector } from "react-redux";
+
+import { incremented, decremented } from "./redux/store";
+
 function App() {
-  return <div className="text-2xl text-red-500">App</div>;
+  const dispatch = useDispatch();
+  const count = useSelector((state) => state.value);
+
+  return (
+    <div>
+      <div className="text-2xl text-red-500">count:{count}</div>
+      <div className="space-x-4">
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          onClick={() => dispatch(incremented())}
+        >
+          +1
+        </button>
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          onClick={() => dispatch(decremented())}
+        >
+          -1
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
